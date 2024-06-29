9a22d24e458c2d5a62f57601b5c8ffa13d6c5c51
harsita <tanuharsita@gmail.com>
2024-06-29 14:54:25 +0530

    all files has been updated

--- my-app/src/screens/Home.js
+++ my-app/src/screens/Home.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card';
 
 export default function Home() {
-  return (
-    <div>
-      <div> <Navbar/> </div>
-      <div>Body</div>
-      <div><Footer/></div>
-    </div>
-  );
+    return (
+        <div>
+            <div> <Navbar /> </div>
+            <div>
+                <Card/>
+            </div>
+           
+            
+            <div><Footer /></div>
+        </div>
+    );
 }
